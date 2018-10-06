ff239efdf5e827cafcb46c84c0d40edb36ed8e01
Ayush Bahuguna <ayushbahuguna8005@gmail.com>
2018-10-06 01:05:15 -0400

    A star resolved 2

--- src/AStar.java
+++ src/AStar.java
@@ -17,8 +17,8 @@ class AStar extends SearchParent {
 		// 11 } };
 		// int initialState[][] = { { 1, 2, 6, 4 }, { 5, 9, 7, 3 }, { 0, 10, 11,
 		// 8 } };
-		//int initialState[][] = { { 0, 11, 10, 9}, { 8, 7, 6, 5 }, { 4, 3, 2, 1 } };
-		int initialState[][] = { { 0, 11, 9, 10 }, { 8, 7, 6, 5 }, { 4, 3, 2, 1 } };
+		int initialState[][] = { { 0, 11, 10, 9}, { 8, 7, 6, 5 }, { 4, 3, 2, 1 } };
+		//int initialState[][] = { { 0, 11, 9, 10 }, { 8, 7, 6, 5 }, { 4, 3, 2, 1 } };
 		int[][] goalState = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 0 } };
 		State root = new State(initialState, goalState);
 		
@@ -53,21 +53,23 @@ class AStar extends SearchParent {
 			State currentState = openList.poll();
 			closedList.add(currentState);
 			currentState.generateChildrenStatesBestFirst();
+			
 			if (currentState.isGoalReached()) {
 				System.out.println("Reached goal state");
 				isGoalReached = true;
 				getSolutionPath(solutionPath, currentState);
 				break;
 			}
+			
 
 			for (int i = 0; i < currentState.childrenStates.size(); i++) {
 				State currentChild = currentState.childrenStates.get(i);
-//				if (currentChild.isGoalReached()) {
-//					System.out.println("Reached goal state");
-//					isGoalReached = true;
-//					getSolutionPath(solutionPath, currentChild);
-//					break;
-//				}
+				if (currentChild.isGoalReached()) {
+					System.out.println("Reached goal state");
+					isGoalReached = true;
+					getSolutionPath(solutionPath, currentChild);
+					break;
+				}
 
 				if (!doesListAlreadyHasChildState(openList, currentChild)
 						&& !doesListAlreadyHasChildState(closedList, currentChild)) {
@@ -78,10 +80,9 @@ class AStar extends SearchParent {
 				if (doesListAlreadyHasChildState(openList, currentChild)) {
 					State stateInList = findExistingChildStateInListAStar(openList, currentChild);
 					if(compareHeuristicManhattanFirstBetter(currentChild, stateInList) && stateInList!=null){
-						//System.out.println("Already in open List current level: "+ currentChild.level + "stateinlist leve: "+ stateInList.level);
 						//System.out.println("Removed "+openList.remove(stateInList)+ " with level and heuristic:" +stateInList.heuristicManhattanDistance+" "+stateInList.level);
-						//openList.add(currentChild);
-						//System.out.println("Added "+openList.add(currentChild)+ " with level and heuristic:" +currentChild.heuristicManhattanDistance+" "+currentChild.level);
+						openList.remove(stateInList);
+						openList.add(currentChild);
 						//System.out.println("Added "+openList.add(currentChild)+ " with level and heuristic:" +currentChild.heuristicManhattanDistance+" "+currentChild.level);
 					}
 				}
